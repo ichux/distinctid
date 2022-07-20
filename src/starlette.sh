4e521fdefbe26a2540b44f13dcd1dc319bdc2ab5
Chukwudi Nwachukwu <theichux@gmail.com>
2022-07-20 22:15:38 +0100

    improved on the content

--- src/starlette.sh
+++ src/starlette.sh
@@ -5,20 +5,19 @@ cd /bin
 # echo -e "\nJSON_RESPONSE = $JSON_RESPONSE\n"
 
 # if [[ $JSON_RESPONSE == "true" ]]; then
-# 	exec gunicorn diid:app --worker-class $GUNICORN_WORKERS \
+# 	exec gunicorn diid:app --workers $GUNICORN_WORKERS \
 # 		-k uvicorn.workers.UvicornWorker \
 # 		--bind 0.0.0.0:8000
 # fi
 
 # if [[ $JSON_RESPONSE == "false" ]]; then
-# 	exec gunicorn diid:app --worker-class $GUNICORN_WORKERS \
+# 	exec gunicorn diid:app --workers $GUNICORN_WORKERS \
 # 		-k uvicorn.workers.UvicornWorker \
 # 		--bind 0.0.0.0:8000
 # fi
 
 # echo "options: 'true' or 'false'" && exit
-
 exec gunicorn diid:app \
-	--worker-class $GUNICORN_WORKERS \
+	--workers `nproc` \
 	-k diid.DistinctIDUvicornWorker \
 	--bind 0.0.0.0:8000
