742fcbb6890d7eb7f8744990f5e5fe44f11633fb
Chukwudi Nwachukwu <theichux@gmail.com>
2022-07-22 10:10:51 +0100

    updates

--- points.py
+++ points.py
@@ -11,9 +11,12 @@ try:
     for ts in re.findall(r"\d\d+", open("graph_data.txt", "r").read()):
         hits.append(int(ts))
 
+    # plt.suptitle("twitter: @zuoike")
     plt.title("Distinct ID")
-    plt.suptitle("twitter: @zuoike")
-    
+
+    plt.xlabel("Amount of times", fontsize=14)
+    plt.ylabel("Requests", fontsize=14)
+
     plt.plot(hits, color="red")
     plt.savefig("points.png")
 except FileNotFoundError:
